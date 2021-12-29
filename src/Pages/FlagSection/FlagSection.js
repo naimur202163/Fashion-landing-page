5f80c2e731904c89cac767dea8d4ce0946c0fde6
unknown <you@example.com>
2021-12-29 15:20:47 +0600

    flag section is added

---
+++ src/Pages/FlagSection/FlagSection.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const FlagSection = () => {
+  return (
+    <div>
+      <Container>
+        <h2>This is Naimuir Rahman D</h2>
+      </Container>
+    </div>
+  );
+};
+
+export default FlagSection;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import Banner from '../Banner/Banner';
-import BannerImg from '../BannerImg/BannerImg';
-import PercentBanner from './../PercentBanner/PercentBanner';
+import React from "react";
+import Banner from "../Banner/Banner";
+import BannerImg from "../BannerImg/BannerImg";
+import FlagSection from "../FlagSection/FlagSection";
+import PercentBanner from "./../PercentBanner/PercentBanner";
 
 const Home = () => {
-    return (
-        <div>
-            <Banner />
-            <PercentBanner />
-            <BannerImg />
-        </div>
-    );
+  return (
+    <div>
+      <Banner />
+      <PercentBanner />
+      <BannerImg />
+      <FlagSection />
+    </div>
+  );
 };
 
 export default Home;
