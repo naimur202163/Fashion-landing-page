4d97ec7617b9989dcfad6cef0a65ae9301e22119
unknown <you@example.com>
2021-12-30 08:28:37 +0600

    flage added

--- src/Pages/FlagSection/FlagSection.js
+++ src/Pages/FlagSection/FlagSection.js
@@ -1,11 +1,26 @@
 import React from "react";
 import { Container } from "react-bootstrap";
-
+import "./FlagSection.css";
+import flagImg from "../../Images/Rectangle 146.png";
 const FlagSection = () => {
   return (
-    <div>
+    <div className="my-5">
       <Container>
-        <h2>This is Naimuir Rahman D</h2>
+        <div style={{ backgroundColor: "#EDDCCB", height: "319px" }}>
+          <div className="flex-containerr">
+            <div className="text">
+              <h3>Start</h3>
+            </div>
+            <div className="box-items"></div>
+            <div className="box-items"></div>
+            <div className="box-items"></div>
+            <div>
+              <img src={flagImg} alt="" />{" "}
+            </div>
+            <div className="box-items"></div>
+            <div className="box-items"></div>
+          </div>
+        </div>
       </Container>
     </div>
   );
