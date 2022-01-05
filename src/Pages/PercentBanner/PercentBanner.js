bb4bdb2547513a899954d6ce08009bbbebb3ff00
unknown <you@example.com>
2022-01-05 19:54:25 +0600

    hoga

--- src/Pages/PercentBanner/PercentBanner.js
+++ src/Pages/PercentBanner/PercentBanner.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
-import './PercentBanner.css'
-import vactor1 from '../../Images/Vector 1.png'
-import vactor2 from '../../Images/Vector 2.png'
+import React from "react";
+import { Container } from "react-bootstrap";
+import "./PercentBanner.css";
+import vactor1 from "../../Images/Vector 1.png";
+import vactor2 from "../../Images/Vector 2.png";
 const PercentBanner = () => {
-    return (
-        <div className='my-5' >
-            <Container>
-
-                <div style={{ height: "auto" }} className='bg-img'>
-                    <div className='flex-container'>
-                        <div className='itmes'> <img src={vactor1} alt="" /></div>
-                        <div></div>
-                        <div> <img src={vactor2} alt="" /></div>
-                    </div>
-                </div>
-            </Container>
-
+  return (
+    <div className="my-5">
+      <Container>
+        <div>
+          <div className="box"></div>
+          {/* Percentise */}
+          <div style={{ height: "auto" }} className="bg-img">
+            <div className="flex-container">
+              <div className="itmes">
+                {" "}
+                <img src={vactor1} alt="" />
+              </div>
+              <div></div>
+              <div>
+                {" "}
+                <img src={vactor2} alt="" />
+              </div>
+            </div>
+          </div>
+          {/* f */}
         </div>
-    );
+      </Container>
+    </div>
+  );
 };
 
 export default PercentBanner;
