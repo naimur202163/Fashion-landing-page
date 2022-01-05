177a3d7b724e32f0f29e1e77fbc299a9e4507bc7
unknown <you@example.com>
2022-01-05 22:40:53 +0600

    upfae

--- src/Pages/PercentBanner/PercentBanner.js
+++ src/Pages/PercentBanner/PercentBanner.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import "./PercentBanner.css";
 import vactor1 from "../../Images/Vector 1.png";
 import vactor2 from "../../Images/Vector 2.png";
@@ -8,7 +8,57 @@ const PercentBanner = () => {
     <div className="my-5">
       <Container>
         <div>
-          <div className="box"></div>
+          <div className="box">
+            <Row>
+              <Col lg={8}>
+                <div style={{ display: "flex" }}>
+                  <h2
+                    style={{
+                      color: "#474747",
+                      marginTop: "2.3rem",
+                      fontWeight: "bold",
+                      fontSize: "3.5rem",
+                      marginLeft: "1rem",
+                    }}
+                  >
+                    Black Friday
+                    <br /> Sale Upto
+                  </h2>
+                  <h1
+                    style={{
+                      color: "#474747",
+                      marginTop: "3.5rem",
+                      marginLeft: "4rem",
+                      fontWeight: "bold",
+                      fontSize: "6rem",
+                    }}
+                  >
+                    60%
+                  </h1>
+                </div>
+              </Col>
+              <Col lg={4}>
+                <div
+                  style={{
+                    textAlign: "center",
+                    marginTop: "6rem",
+                  }}
+                >
+                  <button
+                    style={{
+                      backgroundColor: "#ADABAB",
+                      height: "68px",
+                      width: "320px",
+                      border: "0px",
+                      marginRight: "3rem",
+                    }}
+                  >
+                    <h2> Grab Now !!</h2>
+                  </button>
+                </div>
+              </Col>
+            </Row>
+          </div>
           {/* Percentise */}
           <div style={{ height: "auto" }} className="bg-img">
             <div className="flex-container">
